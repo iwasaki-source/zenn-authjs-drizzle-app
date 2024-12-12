ddb6133aa0e75fe6a83974fa74642238b4897cd8
iwasaki <iwa@cfv.co.jp>
2024-12-13 00:46:29 +0900

    feat: setup t3 env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { env } from "@/env";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p className="text-green-400 font-bold text-8xl">Hello World</p>
       <Button className="my-5" variant='secondary'>Click</Button>
+      {env.NODE_ENV}
     </main>
   );
 }
