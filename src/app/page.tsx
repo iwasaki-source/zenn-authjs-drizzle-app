8e17f4576130932942e7115db51cadf1da405793
iwasaki <iwa@cfv.co.jp>
2024-12-13 00:38:59 +0900

    feat: setup shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p className="text-green-400 font-bold text-8xl">Hello World</p>
+      <Button className="my-5" variant='secondary'>Click</Button>
     </main>
   );
 }
